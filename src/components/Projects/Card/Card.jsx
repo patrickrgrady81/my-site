48eb8e614fe26a3a501171a32c92510a20bf2fe4
Patrick Grady <patrickrgrady81@gmail.com>
2021-06-20 11:14:25 -0400

    Tweaks to Projects page.

--- src/components/Projects/Card/Card.jsx
+++ src/components/Projects/Card/Card.jsx
@@ -3,28 +3,31 @@ import uuid from 'react-uuid';
 
 import './Card.css';
 
+const video = [null, null, null, null, "https://www.youtube.com/watch?v=bmmeJO4AaBg&t=3s", "https://www.youtube.com/watch?v=Ho-NgldkZ7Y&t=2s]", "https://www.youtube.com/watch?v=OLrSQ4sMKso&t=2s", null, "https://www.youtube.com/watch?v=vXUJWl9necw"]
+
 
 const Card = (props) => {
   return (
-    <div className="individual">
-      <div className="name-dec">
+    <div className="individual" id={props.info.name}>
+      <div className="name-desc">
         <div className="name underline">{props.info.name}</div> 
         <div className="desc">{props.info.fullDescription}</div>
       </div>
-      <img className="project-page-image" src={props.image} alt="" />
+      {(props.info.id !== 0 && props.info.id !== 1 && props.info.id !== 2 && props.info.id !== 3 && props.info.id !== 7) ?<a class="tooltips_cls" href={video[props.info.id]} target="_blank" rel="noopener noreferrer"><img className="project-page-image" src={props.image} alt="" /><span>Click for Video Demo</span></a> : <img className="project-page-image" src={props.image} alt="" />}
+      {/* <a class="tooltips_cls" href="#"><span>Click for Brief Youtube Demo</span><img className="project-page-image" src={props.image} alt="" /></a>*/}
       <div className="languages">
         <div className="lang">Languages</div>
         <ul className="lang-list">
           {props.info.languages.map(lang => {
-            return <li className="lang-item" key={uuid()}>{lang}</li>
+            return <li key={uuid()} className="li">{lang}</li>
           })}
         </ul>
       </div>
       <div className="demo-code">
-      <div className="links">Start Backend: <a href={props.info.demoBack}>{props.info.demoBack}</a></div>
-      <div className="links">Start Frontend: <a href={props.info.demoFront}>{props.info.demoFront}</a></div>
-      <div className="links">Backend code: <a href={props.info.back}>{props.info.back}</a></div>
-      <div className="links">Backend code: <a href={props.info.front}>{props.info.front}</a></div>
+      <div>Start Backend: <a href={props.info.demoBack} className="ilinks">{props.info.demoBack}</a></div>
+      <div>Start Frontend: <a href={props.info.demoFront} className="ilinks">{props.info.demoFront}</a></div>
+      <div>Backend code: <a href={props.info.back} className="ilinks">{props.info.back}</a></div>
+      <div>Backend code: <a href={props.info.front} className="ilinks">{props.info.front}</a></div>
       </div>
     </div>
   )
